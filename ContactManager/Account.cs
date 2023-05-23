06bc3ae1a637463000ed86a351bf0465e6389981
haciek <maciekhab@gmail.com>
2023-05-23 22:25:10 +0200

    Added login/signup for multiple accounts, refactored to 2 seperate windows

---
+++ ContactManager/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager
+{
+    public class Account
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public Account(string login, string password, string email)
+        {
+            this.login = login;
+            this.password = password;
+            this.email = email;
+        }
+        public bool isValid() => !(login.Equals(string.Empty) || password.Equals(string.Empty) || email.Equals(string.Empty));
+        public bool Matches(string login, string password)
+        {
+            return (this.login != null && this.password != null)
+                ? this.login.Equals(login) && this.password.Equals(password)
+                : false;
+
+        }
+
+    }
+}
---
+++ ContactManager/LoginWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace ContactManager
+{
+
+    public partial class LoginWindow : Window
+    {
+        private string accountsDirPath;
+        private string accountsFilePath;
+        private List<Account>? accounts;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            this.accountsDirPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            // TODO: check if file exists
+            this.accountsFilePath = System.IO.Path.Combine(this.accountsDirPath, "file.json");
+
+            string jsonString = File.ReadAllText(this.accountsFilePath);
+            this.accounts = JsonConvert.DeserializeObject<List<Account>>(jsonString);
+        }
+        private bool isAccountAvailable(Account newAcc)
+        {
+            if (this.accounts == null)
+                return false;
+
+            foreach (var acc in this.accounts)
+            {
+                if (acc.login == newAcc.login)
+                    return false;
+            }
+
+            return true;
+        }
+        private void ShowSignUp(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Collapsed;
+            SingUpPanel.Visibility = Visibility.Visible;
+
+            LoginFieldSignUp.Text = string.Empty;
+            PasswordFieldSignUp.Password = string.Empty;
+            EmailFieldSignUp.Text = string.Empty;
+        }
+        private void ShowLogin(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Visible;
+            SingUpPanel.Visibility = Visibility.Collapsed;
+
+            LoginField.Text = string.Empty;
+            PasswordField.Password = string.Empty;
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            bool accExists = false;
+            if (this.accounts == null)
+                return;
+
+            foreach (var acc in this.accounts)
+            {
+                if (acc == null) continue;
+
+                if (acc.Matches(LoginField.Text.ToString(), PasswordField.Password.ToString()))
+                {
+                    accExists = true;
+                    break;
+                }
+            }
+            if (!accExists)
+            {
+                ShowLogin(sender, e);
+                MessageBox.Show("Login Failed");
+                return;
+            }
+
+            MessageBox.Show("Login Succesfull");
+            this.Close();
+        }
+
+        private void SignUp(object sender, RoutedEventArgs e)
+        {
+            ShowLogin(sender, e);
+            Account user = new(LoginFieldSignUp.Text, PasswordFieldSignUp.Password, EmailFieldSignUp.Text);
+            if (!user.isValid())
+            {
+                ShowSignUp(sender, e);
+                MessageBox.Show("Error", "Invalid Field");
+                return;
+            }
+            if (!isAccountAvailable(user))
+            {
+                MessageBox.Show("Error", "Account Taken");
+                return;
+            }
+
+            if (this.accounts != null)
+            {
+                this.accounts.Add(user);
+                string jsonString = JsonConvert.SerializeObject(this.accounts);
+                MessageBox.Show("Signed in");
+                File.WriteAllText(this.accountsFilePath, jsonString);
+                return;
+            }
+            MessageBox.Show("Error", "Failed To Sign In");
+        }
+    }
+}
